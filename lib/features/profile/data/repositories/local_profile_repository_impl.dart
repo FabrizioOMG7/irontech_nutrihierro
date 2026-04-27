d8097dcc93c73e3a6ea346e34644eccbccb6aa0a
Fabrizio <fabriziohoyosob@gmail.com>
2026-04-26 21:48:44 -0500

    base de datos act

--- lib/features/profile/data/repositories/local_profile_repository_impl.dart
+++ lib/features/profile/data/repositories/local_profile_repository_impl.dart
@@ -1,7 +1,8 @@
+import 'package:irontech_nutrihierro/core/data/local_db/d/isar_child.dart';
 import 'package:isar/isar.dart';
+
 import '../../domain/child.dart';
 import '../../domain/repositories/profile_repository.dart';
-import '../../../../core/data/local_db/models/isar_child.dart';
 
 class LocalProfileRepositoryImpl implements ProfileRepository {
   final Isar isar;
@@ -14,10 +15,12 @@ class LocalProfileRepositoryImpl implements ProfileRepository {
       ..childId = child.id
       ..name = child.name
       ..birthDate = child.birthDate
-      ..gender = child.gender.name;
+      ..gender = child.gender.name; // Convertimos Gender a String
 
     await isar.writeTxn(() async {
-      await isar.isarChilds.putByChildId(isarChild);
+      // Alternativa a putByIndex: eliminar si existe y luego agregar
+      await isar.isarChilds.filter().childIdEqualTo(child.id).deleteAll();
+      await isar.isarChilds.put(isarChild);
     });
   }
 
@@ -25,20 +28,19 @@ class LocalProfileRepositoryImpl implements ProfileRepository {
   Future<List<Child>> getChildren() async {
     final isarChildren = await isar.isarChilds.where().findAll();
 
-    return isarChildren.map((isarChild) {
-      return Child(
-        id: isarChild.childId,
-        name: isarChild.name,
-        birthDate: isarChild.birthDate,
-        gender: genderFromStorage(isarChild.gender),
-      );
-    }).toList();
+    return isarChildren.map((isarChild) => Child(
+      id: isarChild.childId,
+      name: isarChild.name,
+      birthDate: isarChild.birthDate,
+      gender: genderFromStorage(isarChild.gender), // Convertimos String a Gender
+    )).toList();
   }
 
   @override
   Future<void> deleteChild(String id) async {
     await isar.writeTxn(() async {
-      await isar.isarChilds.deleteByChildId(id);
+      // Alternativa a deleteByIndex: filtrar y eliminar
+      await isar.isarChilds.filter().childIdEqualTo(id).deleteAll();
     });
   }
 }
